93bc0f6a3965ec732296befa9a481a732409ab11
Administrator <Administrator@FCHS-101-FZ3TQW.PCS.local>
2016-11-14 11:10:13 -0500

    Allowed writing onto disk

--- FBLA/src/Employee.java
+++ FBLA/src/Employee.java
@@ -1,7 +1,21 @@
+import java.io.BufferedWriter;
 import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.FileWriter;
+import java.io.IOException;
 public class Employee {
-	public Employee(String name, String startTime, String endTime){
+	public Employee(String name, String startTime, String endTime) throws IOException{
+		File file = new File("C:/data.txt");
+		if(!file.exists()){
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			FileWriter fr = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fr);
+			bw.write("Constant");
+			bw.close();
+			
+		}
 	}
 }
--- FBLA/src/Main.java
+++ FBLA/src/Main.java
@@ -1,7 +1,9 @@
+import java.io.IOException;
 import java.util.Scanner;
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		Employee em = new Employee("Hi","Hey","howdy");
 		Scanner input = new Scanner(System.in);
 		int option = 5;
 		
